e456f1663dfd5920df2e6ac9ea12afccfdab8c87
yigitturkan <yigit.trkkan2003@gmail.com>
2026-03-24 16:41:41 +0300

    Update 03 24.03.2026

---
+++ Web/Scripts/Login.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    // Hatırlanan mail
+    const rememberedEmail = localStorage.getItem('userEmail');
+    if (rememberedEmail) {
+        $('#Email').val(rememberedEmail);
+        $('#RememberMe').prop('checked', true);
+    }
+
+    // Submit
+    $('form').on('submit', function () {
+        const rememberMe = $('#RememberMe').is(':checked');
+        const email = $('#Email').val();
+
+        if (rememberMe) {
+            localStorage.setItem('userEmail', email);
+        } else {
+            localStorage.removeItem('userEmail');
+        }
+    });
+
+});
